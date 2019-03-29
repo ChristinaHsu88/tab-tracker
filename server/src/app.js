2ae5aba237fe7471360415c94b630b1122a35b58
ChristinaHsu88 <christina.hsu.ca@gmail.com>
2019-03-29 13:51:33 -0700

    set up initial express routing

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,17 @@
-console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser') /* process json data */
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors()) /* allow any clients in the world to access the domain */
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello word'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
