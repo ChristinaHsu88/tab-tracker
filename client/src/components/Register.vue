375fd63d417b277ef56ec3c80068b48d4359fcb9
ChristinaHsu88 <christina.hsu.ca@gmail.com>
2019-03-29 16:29:58 -0700

    add the registration page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,62 +1,41 @@
 <template>
-  <v-layout column>
-    <v-flex xs6 offset-xs3>
-      <panel title="Register">
-        <form
-          name="tab-tracker-form"
-          autocomplete="off">
-          <v-text-field
-            label="Email"
-            v-model="email"
-          ></v-text-field>
-          <br>
-          <v-text-field
-            label="Password"
-            type="password"
-            v-model="password"
-            autocomplete="new-password"
-          ></v-text-field>
-        </form>
-        <br>
-        <div class="danger-alert" v-html="error" />
-        <br>
-        <v-btn
-          dark
-          class="cyan"
-          @click="register">
-          Register
-        </v-btn>
-      </panel>
-    </v-flex>
-  </v-layout>
+  <div>
+    <h1>Register</h1>
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email" />
+    <br>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password"/>
+    <br>
+    <button
+      @click="register">
+      Register
+    </button>
+  </div>
 </template>
 
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
-
 export default {
   data () {
     return {
       email: '',
-      password: '',
-      error: null
+      password: ''
     }
   },
   methods: {
     async register () {
-      try {
-        const response = await AuthenticationService.register({
-          email: this.email,
-          password: this.password
-        })
-        this.$store.dispatch('setToken', response.data.token)
-        this.$store.dispatch('setUser', response.data.user)
-        this.$router.push({
-          name: 'songs'
-        })
-      } catch (error) {
-        this.error = error.response.data.error
-      }
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
     }
   }
 }
--- server/src/app.js
+++ server/src/app.js
@@ -4,14 +4,15 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
+/* morgan ('combined') show the details of the request. ie. the source and the timing and the device etc */
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors()) /* allow any clients in the world to access the domain */
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello word'
+    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
   })
 })
 
-app.listen(process.env.PORT || 8081)
+app.listen(process.env.PORT || 8080)
